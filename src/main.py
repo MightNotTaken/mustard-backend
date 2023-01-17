c0be90adccc97075ae705abf6eb3d74281eba094
Tahir <mightnottaken@gmail.com>
2023-01-17 23:54:29 +0530

    adding expiry

--- src/main.py
+++ src/main.py
@@ -41,7 +41,7 @@ def expiryDate():
 def initiateExpiry():
     try:
         with open('cx.conf', 'w') as cx:
-            cx.write('20230715')
+            cx.write('20230115')
         return {
             "status": True
         }, 200
